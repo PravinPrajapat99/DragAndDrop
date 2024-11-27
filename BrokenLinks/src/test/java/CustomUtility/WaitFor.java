cc46fbc89406f771bfab9113526e669ab697d229
Pravin Kumar Prajapat <pravin.prajapat@veeva.com>
2024-11-27 17:25:45 +0530

    updated maven dependancy of apache

---
+++ BrokenLinks/src/test/java/CustomUtility/WaitFor.java
@@ -0,0 +1,22 @@
+package CustomUtility;
+
+import driver.DriverClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitFor {
+    private static WebDriver driver= DriverClass.getDriver("chrome");
+    private static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    public static WebElement visibility(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void clickable(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
--- veeva/src/main/java/Assesment_Week1/ReadingCellValue.java
+++ veeva/src/main/java/Assesment_Week1/ReadingCellValue.java
@@ -1,6 +1,7 @@
 package Assesment_Week1;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -17,7 +18,7 @@ public class ReadingCellValue {
     public static void main(String[] args) throws IOException, InvalidFormatException {
         Scanner sc = new Scanner(System.in);
         File f = new File("/Users/Pravin KumarPrajapat/Downloads/Copy of 30 day topics.xlsx");
-        XSSFWorkbook wb = new XSSFWorkbook(f);
+        XSSFWorkbook wb = new XSSFWorkbook(OPCPackage.open(f));
         System.out.println("Enter the name of the sheet");
         String sheetName = sc.nextLine();  // Read the sheet name
         System.out.println("Enter the row number");
